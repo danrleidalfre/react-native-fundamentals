3871bdd495c7d4ffa0ff46243c14fcef8f4adf84
Danrlei <danrleidalfre@gmail.com>
2024-08-07 12:30:00 -0300

    add name type to list

--- src/screenn/Home/index.tsx
+++ src/screenn/Home/index.tsx
@@ -1,24 +1,29 @@
 import {Alert, FlatList, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {styles} from "./styles";
 import {Participant} from "../../components/Participant";
+import {useState} from "react";
 
 export default function Home() {
-  const participants = ['Danrlei']
+  const [participants, setParticipants] = useState<string[]>([])
+  const [name, setName] = useState<string>('')
 
   function handleAddParticipant() {
-    Alert.alert('Novo', 'Adicionando novo')
+    if (!name) {
+      return
+    }
+
+    if (participants.includes(name)) {
+      return Alert.alert('Participante já adicionado')
+    }
+
+    setParticipants(prevState => [name, ...prevState])
+    setName('')
   }
 
   function handleRemoveParticipant() {
     Alert.alert('Remover Participante', 'Tem certeza que deseja remover esse participante?', [
-      {
-        text: 'Sim',
-        onPress: () => onDelete()
-      },
-      {
-        text: 'Não',
-        style: 'cancel'
-      }
+      {text: 'Sim', onPress: () => onDelete()},
+      {text: 'Não', style: 'cancel'}
     ])
   }
 
@@ -28,13 +33,22 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder='Name participant'/>
-        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+        <TextInput
+          value={name}
+          onChangeText={setName}
+          style={styles.input}
+          placeholder='Nome do participante'
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleAddParticipant}
+        >
           <Text style={styles.textButton}>add</Text>
         </TouchableOpacity>
       </View>
       <FlatList
         data={participants}
+        keyExtractor={item => item}
         renderItem={({item}) => (
           <Participant name={item} onRemove={handleRemoveParticipant}/>
         )}
